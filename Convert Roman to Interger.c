2b0802cad4985ca80d9e61bbc6d3c6b20f17ae92
mind bender <98180582+Tharun-DV@users.noreply.github.com>
2023-03-09 12:29:51 +0530

    Add files via upload

---
+++ Convert Roman to Interger.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+static int roman_to_integer(char c)
+{
+    switch(c) {
+    case 'I':  
+        return 1;  
+    case 'V':  
+        return 5;  
+    case 'X':  
+        return 10;  
+    case 'L':  
+        return 50;  
+    case 'C':  
+        return 100;  
+    case 'D':  
+        return 500;  
+    case 'M':  
+        return 1000;  
+    default:
+        return 0;
+    }
+}
+
+int roman_to_int (char *s)
+{
+    int i, int_num = roman_to_integer(s[0]);
+
+    for (i = 1; s[i] != '\0'; i++) {
+        int prev_num = roman_to_integer(s[i - 1]);
+        int cur_num = roman_to_integer(s[i]);
+        if (prev_num < cur_num) {
+            int_num = int_num - prev_num + (cur_num - prev_num);
+        } else {
+            int_num += cur_num;
+        }
+    }
+    return int_num;
+}
+int main(void)
+ {
+  char *str1 = "XIV";
+    printf("Original Roman number: %s", str1);
+    printf("\nRoman to integer: %d", roman_to_int(str1));
+    return 0;
+}
