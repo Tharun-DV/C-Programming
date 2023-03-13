d3d98f75dbf2747cd420a4bcf13cb06e32a6b41d
mind bender <98180582+Tharun-DV@users.noreply.github.com>
2023-03-13 11:53:43 +0530

    Add files via upload

---
+++ reallocate.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main(){
+	int num;
+	int *p;
+	p = (int*)malloc(5*sizeof(int)); 
+	printf("Enter No of Memory bytes to allocate:");
+	scanf("%d",&num);
+	printf("size before : %zu\n" ,sizeof(*p));
+	p = (int*)realloc(p, 11*sizeof(int));
+	printf("size after realloc %zu\n",sizeof(*p));
+
+	int y;
+	printf("Do you want to free the memory!? :\n(0/1)==> ");
+	scanf("%d",&y);
+	if (y==1){
+		free(p);
+		printf("Freed Memory %zu",sizeof(*p));
+	}
+}
